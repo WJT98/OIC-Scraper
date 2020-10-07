5c91d0809adcaa6b966081357374d14d6a7c55b4
wjt98 <williamjt1998@gmail.com>
2020-10-07 15:41:44 -0400

    Extracts data from all the expiry dates from the html

--- OICScraper.py
+++ OICScraper.py
@@ -16,7 +16,8 @@ def get_cnt():
 
 def get_html(ticker, date):
 	cnt = get_cnt()
-	url = "https://oic.ivolatility.com/oic_adv_options.j?cnt="+cnt+"&ticker="+ticker+"&exp_date=1"
+	url = "https://oic.ivolatility.com/oic_adv_options.j?cnt="+cnt+"&ticker="+ticker+"&exp_date=-1"
+	print(url)
 	page = requests.get(url)
 	soup = BeautifulSoup(page.content, 'html.parser')
 	filename = os.path.join(os.getcwd(),ticker+"/"+date+".html")
@@ -25,17 +26,21 @@ def get_html(ticker, date):
 		f.write(str(soup))
 
 def store_data(ticker, date):
-	with open(ticker+"/"+date+".html", 'r') as f:
+	with open("SPY"+"/"+date+".html", 'r', encoding='utf-8') as f:
 		contents = f.read()
-		soup = BeautifulSoup(contents, 'lxml')
-	table = soup.find_all('table')[11]
-	return pd.read_html(str(table), flavor='bs4', header=[0])[0]
-	# with open("RAWHTML.html", "w", encoding='utf-8') as f:
-	# 	f.write(str(table))
-	# 	pd.set_option("display.max_rows", None, "display.max_columns", None)
-	# 	f.write(str(df))
+	soup = BeautifulSoup(contents, 'lxml')
+	table = soup.find_all('table')
+	df =[]
+	expiry_dates = soup.find_all(text=re.compile("Expiry: [A-Za-z]{3} [0-9]{2}, [0-9]{4}"))
+	for i in range(len(expiry_dates)):
+		expiry_dates[i] = expiry_dates[i][0:20]
+	print(expiry_dates)
+	df.append(pd.DataFrame(expiry_dates))
+	for i in range(1,len(table)):
+		if len(table[i].find_all(text=re.compile("Gamma"))) > 0:
+			df.append(pd.read_html(str(table[i]), flavor='bs4', header=[1])[0])
+	return df
 
-	
 def main():
 	today = date.today()
 	d = today.strftime("%Y-%m-%d")
@@ -43,5 +48,9 @@ def main():
 	if not os.path.exists(filename):
 		get_html("SPY", d)
 	df = store_data("SPY", d)
+	col = list(df[-1].columns.values) 
+	print(df[0].head())
+	print(df[1].head())
 
+	
 main()
